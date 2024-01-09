1177980a5c09b8b4d792bc7a31d20dc66262951c
Parvesh M <parvesh@gmail.com>
2024-01-09 17:19:32 +0530

    Fire base configuration done, user data stored in context

--- Olx_Clone/src/main.jsx
+++ Olx_Clone/src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom/client";
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+// context for accessing the firebase
+import { FirebaseContext } from "./context/context";
+import firebase from "./fireBase/config";
+// context for User details
+import Context from "./context/context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <Context>
+        <RouterProvider router={router} />
+      </Context>
+    </FirebaseContext.Provider>
   </React.StrictMode>
 );
